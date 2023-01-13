87128033b0a6b36da266e4f8ab6f341077088c55
Sentientnapkin <73544597+Sentientnapkin@users.noreply.github.com>
2023-01-13 14:23:14 -0800

    DFS and BFS

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -1,7 +1,6 @@
 package Graph;
 
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Graph {
     HashMap<String, Vertex> vertices;
@@ -50,6 +49,51 @@ public class Graph {
         Vertex vertex2 = vertices.get(label2);
         vertex1.neighbors.remove(vertex2);
         vertex2.neighbors.remove(vertex1);
+    }
+
+    public boolean depthFirstSearch(String s, String t) {
+        HashSet<Vertex> explored = new HashSet<>();
+        Deque<Vertex> stack = new LinkedList<>();
+        stack.push(vertices.get(s));
+
+        while(!stack.isEmpty()){
+            Vertex n = stack.pop();
+            if(!explored.contains(n)){
+                explored.add(n);
+                System.out.println(n.label);
+                for(Vertex v: n.neighbors){
+                    if(v==vertices.get(t)){
+                        return true;
+                    }
+                    stack.push(v);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean breadthFirstSearch(String s, String t) {
+        HashSet<Vertex> visited = new HashSet<>();
+        Queue<Vertex> queue = new LinkedList<>();
+        visited.add(vertices.get(s));
+        queue.offer(vertices.get(s));
+
+        while(!queue.isEmpty()){
+            Vertex n = queue.poll();
+            System.out.println(n.label);
+            for(Vertex v: n.neighbors){
+                if(v==vertices.get(t)){
+                    return true;
+                }
+                if(!visited.contains(v)){
+                    visited.add(v);
+                    queue.offer(v);
+                }
+            }
+        }
+
+        return false;
     }
 
     public void printGraph() {
@@ -189,6 +233,125 @@ public class Graph {
         System.out.println("\nGot:");
         graph.removeVertex("London");
         graph.printGraph();
+        System.out.println();
+
+        // --------------------------
+        // Test 6: DFS True
+        // --------------------------
+        System.out.println("-------------------");
+        System.out.println("Test 6: DFS True");
+        System.out.println("Expected:");
+        System.out.println("A\n" +
+                "B\n" +
+                "E\n" +
+                "F\n" +
+                "C\n" +
+                "true");
+
+        System.out.println("\nGot:");
+
+        Graph graph2 = new Graph();
+        graph2.addVertex("A");
+        graph2.addVertex("B");
+        graph2.addVertex("C");
+        graph2.addVertex("D");
+        graph2.addVertex("E");
+        graph2.addVertex("F");
+        graph2.addVertex("G");
+        graph2.addVertex("H");
+        graph2.addVertex("I");
+        graph2.addEdge("A", "D");
+        graph2.addEdge("A", "C");
+        graph2.addEdge("A", "B");
+        graph2.addEdge("D", "I");
+        graph2.addEdge("D", "H");
+        graph2.addEdge("C", "G");
+        graph2.addEdge("B", "F");
+        graph2.addEdge("B", "E");
+
+        System.out.println(graph2.depthFirstSearch("A", "G"));
+        System.out.println();
+
+        // --------------------------
+        // Test 7: DFS False
+        // --------------------------
+        System.out.println("-------------------");
+        System.out.println("Test 7: DFS False");
+        System.out.println("Expected:");
+        System.out.println("A\n" +
+                "B\n" +
+                "E\n" +
+                "F\n" +
+                "C\n" +
+                "G\n" +
+                "D\n" +
+                "H\n" +
+                "I\n" +
+                "false");
+
+        System.out.println("\nGot:");
+
+        System.out.println(graph2.depthFirstSearch("A", "Z"));
+        System.out.println();
+
+        // --------------------------
+        // Test 8: BFS True
+        // --------------------------
+        System.out.println("-------------------");
+        System.out.println("Test 8: BFS True");
+        System.out.println("Expected:");
+        System.out.println("A\n" +
+                "B\n" +
+                "C\n" +
+                "true");
+
+        System.out.println("\nGot:");
+
+        // graph3 is the same as graph3, but the
+        // neighbor lists are reversed
+
+        Graph graph3 = new Graph();
+        graph3.addVertex("A");
+        graph3.addVertex("B");
+        graph3.addVertex("C");
+        graph3.addVertex("D");
+        graph3.addVertex("E");
+        graph3.addVertex("F");
+        graph3.addVertex("G");
+        graph3.addVertex("H");
+        graph3.addVertex("I");
+        graph3.addEdge("A", "B");
+        graph3.addEdge("A", "C");
+        graph3.addEdge("A", "D");
+        graph3.addEdge("D", "H");
+        graph3.addEdge("D", "I");
+        graph3.addEdge("C", "G");
+        graph3.addEdge("B", "E");
+        graph3.addEdge("B", "F");
+
+        System.out.println(graph3.breadthFirstSearch("A", "G"));
+        System.out.println();
+
+        // --------------------------
+        // Test 9: BFS False
+        // --------------------------
+        System.out.println("-------------------");
+        System.out.println("Test 9: BFS False");
+        System.out.println("Expected:");
+        System.out.println("A\n" +
+                "B\n" +
+                "C\n" +
+                "D\n" +
+                "E\n" +
+                "F\n" +
+                "G\n" +
+                "H\n" +
+                "I\n" +
+                "false");
+
+        System.out.println("\nGot:");
+
+        System.out.println(graph3.breadthFirstSearch("A", "Z"));
         System.out.println();
     }
 }
